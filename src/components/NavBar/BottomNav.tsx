e9b5ae661b0fb532c276b5520b3946654d3bb4eb
Henri baruki <baruki.henri@aluno.ifsp.edu.br>
2026-03-26 16:35:08 -0300

    tudo certo

--- src/components/NavBar/BottomNav.tsx
+++ src/components/NavBar/BottomNav.tsx
@@ -75,6 +75,12 @@ export default function BottomNav() {
                     className="font-golos flex items-center gap-1 font-bold text-[var(--black)] transition-colors hover:text-[var(--second)]"
                   >
                     {link.label}
+                    <Image 
+                      src={'/Menu-dot.svg'}
+                      alt='menu'
+                      width={10}
+                      height={10}/>
+                      
                   </button>
 
                   <div className='absolute left-0 top-full hidden min-w-[180px] rounded-lg border-grav-100 bg-white p-2 shadow-xl group-hover:block'>
@@ -85,6 +91,9 @@ export default function BottomNav() {
                         className='font-golos block rounded-md px-4 py-2 text-[var(--black)] transition-colors hover:bg-var(--prim-light)'>
                         {item.label}
                       </Link>
+
+  
+
                     ))}
                   </div>
                 </div>
@@ -92,7 +101,7 @@ export default function BottomNav() {
                 <Link
                   key={link.label}
                   href={link.href}
-                  className="font-golos font-bold text-[var(--black)] transition-colors hover:text-[var(--second)]"
+                  className="font-golos font-bold text-[var(--black)] transition-colors hover:text-[var(--second)] gap-1 flex"
                 >
                   {link.label}
                   <Image 
