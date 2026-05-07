35996724195d01629135d10360c0b26aca0c398e
Henri baruki <baruki.henri@aluno.ifsp.edu.br>
2026-05-07 17:05:29 -0300

    Swipper do Cart e Wishlist

--- src/app/UI-Components/index.tsx
+++ src/app/UI-Components/index.tsx
@@ -1,11 +1,13 @@
 
 
 import Hero from "./Hero/Hero";
+import Trending from "./Index/Trending/page";
 
 export default function Index(){
     return(
         <div>
             < Hero/>
+            < Trending/>
         </div>
     )
 }
